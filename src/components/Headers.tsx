abf8c10e5840c322999f47fdcc6653e932f9e1e3
deivdf <dfdavidfernando@gmail.com>
2024-11-05 21:12:27 -0500

    change: se agrega nuva funcinalidad al header

--- src/components/Headers.tsx
+++ src/components/Headers.tsx
@@ -1,7 +1,13 @@
-import {NavLink} from 'react-router-dom'
+import { useMemo } from 'react'
+import {NavLink, useLocation} from 'react-router-dom'
 export default function Headers() {
+  const location = useLocation()
+  console.log(location.pathname)
+  const isHome = useMemo(()=> location.pathname === '/', [location.pathname])
+  console.log(isHome)
+  //bg-header es una clase de tailwind que se crea en el archivo tailwind.config.js para agregar un background image
   return (
-    <header className="bg-slate-800">
+    <header className={isHome ? "bg-header bg-center bg-cover" : "bg-slate-800"}>
         <div className="mx-auto container px-5 py-16">
             <div className="flex items-center justify-between">
               <div>
@@ -13,6 +19,49 @@ export default function Headers() {
                 <NavLink className={({isActive})=> isActive ? "text-orange-500 hover:text-orange-500 font-bold px-4": "text-white hover:text-orange-500 font-bold px-4"} to="/Favorite">Favorite</NavLink>
               </nav>
             </div>
+            { isHome && (
+              <form
+                className='md:w-1/2 2xl:w-1/3 bg-cyan-600 my-24 p-10 rounded-lg shadow space-y-6'
+              >
+                <div className="space-y-4">
+                  <label 
+                    htmlFor="ingredient"
+                    className="block text-white uppercase font-extrabold text-lg"
+                  >
+                    Nombre o ingredientes
+                  </label>
+                  <input
+                  type="text"
+                  id="ingredient"
+                  name='ingredient'
+                  className="p-3 focus:outline-none rounded-lg w-full"
+                  placeholder="Busca por ingredientes o nombre Ej. Vodka, Tequila, Cafe"
+                  />
+
+                </div>
+                <div className="space-y-4">
+                  <label 
+                    htmlFor="ingredient"
+                    className="block text-white uppercase font-extrabold text-lg"
+                  >
+                    Selecione Categora
+                  </label>
+                  <select
+                  id="ingredient"
+                  name='ingredient'
+                  className="p-3 focus:outline-none rounded-lg w-full"
+                  >
+                  <option value="">-- Seleccione Categoria --</option>
+                  </select>
+                </div>
+                <input 
+                  type="submit"
+                  value="Buscar Recetas"
+                  className="bg-orange-500 hover:bg-orange-600 cursor-pointer text-white font-extrabold w-full p-2 rounded-lg uppercase"
+
+                />
+              </form>
+            )}
         </div>
     </header>
   )
