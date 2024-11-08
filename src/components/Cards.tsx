56756258b3a42082615c3933cb4066e7c89b0b88
deivdf <dfdavidfernando@gmail.com>
2024-11-08 11:30:33 -0500

    add: nuevo componente Cards para mostrar bebidas y sus animaciones

---
+++ src/components/Cards.tsx
@@ -0,0 +1,26 @@
+import { Drinks } from "../types/type"
+
+type CardsProps = {
+    drinks: Drinks
+}
+export default function Cards({drinks}: CardsProps) {
+  return (
+    <div className="container grid grid-cols-1 md:grid-cols-3">
+        {drinks.drinks.map((item)=>(
+          <div key={item.idDrink} className="bg-slate-50 text-center p-5 m-5 w-10/12 shadow-md rounded-lg overflow-hidden">
+            <div className="overflow-hidden">
+                <img 
+                    src={item.strDrinkThumb} 
+                    alt={`Imagen de ${item.strDrink}`}
+                    className="hover:scale-150 translate-x-0 hover:rotate-6 transition-transform duration-300 ease-in-out hover:ease-out hover:duration-300 h-auto object-cover object-center"
+                />
+            </div>
+            <div className="p-2 bg-cyan-100 rounded-lg my-5"> 
+                <h2 className="font-black text-2xl truncate">{item.strDrink}</h2>
+            </div>
+            <button className="bg-cyan-200 p-2 my-3 rounded-lg hover:bg-cyan-500 hover:text-white w-full font-bold">Ver receta</button>
+          </div>
+        ))}
+    </div>
+  )
+}
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,8 +1,20 @@
+import Cards from "../components/Cards"
+import { useAppStore } from "../store/useAppStore"
 
 export default function Home() {
+  const dirksResponse = useAppStore((state) => state.drinks)
+  //sepude hacer con useMemo pero lo voy a dejar asi
+
   return (
-    <div>
-        <h1>Inicio</h1>
-    </div>
+    <>
+        {dirksResponse.drinks.length===0 ? (<div><h1 className="font-semibold text-center text-6xl">No hay recetas</h1></div>): (
+          <div>
+            <h1 className="font-semibold text-center text-6xl">Recetas de Bebidas</h1>
+            <Cards
+              key={dirksResponse.drinks[0].idDrink}
+              drinks={dirksResponse}
+            />
+        </div>)}
+    </>
   )
 }
