ee31289cc82f6e3341bf4ec343961f62c1a1f6e6
deivdf <dfdavidfernando@gmail.com>
2024-12-12 17:13:09 -0500

    add: se agraga funcionalidad para la optimizacion de la pagina

--- src/router.tsx
+++ src/router.tsx
@@ -1,15 +1,25 @@
+import {lazy, Suspense} from 'react'
 import {Route, Routes, BrowserRouter} from 'react-router-dom'
-import Home from './pages/Home'
+// lo mismo que abajo
+const Home = lazy(()=> import('./pages/Home'))
 import Recetas from './pages/Recetas'
-import FavoritePage from './pages/FavoritePage'
 import Layout from './layouts/Layouts'
+//import FavoritePage from './pages/FavoritePage' se replaza para optimizar la pagina y que no se cargue todo de una vez
+const FavoritePage = lazy(()=> import('./pages/FavoritePage'))
 export default function Approuter() {
   return (
     <BrowserRouter>
       <Routes>
         <Route element={<Layout />}>
-          <Route path='/' element={<Home/>} index/>
-          <Route path='/favorite' element={<FavoritePage/>} />
+          <Route path='/' element={
+            <Suspense fallback="Cargando...">
+              <Home/>
+            </Suspense>
+          } index/>
+          <Route path='/favorite' element={
+            <Suspense fallback="Cargando...">
+              <FavoritePage/>
+          </Suspense>} />
           <Route path='/recetas' element ={<Recetas/>}/>
         </Route>
       </Routes>
